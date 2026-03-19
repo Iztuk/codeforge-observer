cf9a45e5cf5406624ad9a460ad1414d04c216fdb
Iztuk <johnandrew.kutz@gmail.com>
2026-03-18 19:20:41 -0500

    fix(audit) added default response fallback for status code matching

--- audit/audit.go
+++ audit/audit.go
@@ -127,7 +127,10 @@ func AuditResponse(r *http.Response, op *OpenApiOperation, components *OpenApiCo
 
 	res, ok := op.Responses[strconv.Itoa(r.StatusCode)]
 	if !ok {
-		findings = append(findings, fmt.Errorf("response status code not defined: %d", r.StatusCode))
+		res, ok = op.Responses["default"]
+		if !ok {
+			findings = append(findings, fmt.Errorf("response status code not defined: %d", r.StatusCode))
+		}
 		return findings
 	}
 
